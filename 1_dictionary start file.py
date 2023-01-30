efb6cc4b2222dee69b8d96ca6091b0a41818495d
Shep <shepardjberry@gmail.com>
2023-01-30 14:09:41 -0600

    initial commit

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -6,6 +6,7 @@ phonebook = {'Chris':'555−1111',
 
 
 
+"""
 print()
 print('*****  start section 1 - print dictionary ********')
 print()
@@ -19,15 +20,19 @@ print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
 print('*****  start section 2 - search dictionary ********')
 print()
 
+name = "Chris"
 
-
+if name in phonebook:
+    print(phonebook[name])
+else:
+    print(f'{name} does not exist in the phonebook')
 
 
 
@@ -46,7 +51,12 @@ print()
 print('*****  start section 3 - edit/append dictionary ********')
 print()
 
+print(phonebook)
 
+phonebook["Chris"] = "555-4444"
+phonebook["Joe"] = "555-0123"
+
+print(phonebook)
 
 
 
@@ -63,7 +73,9 @@ print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
-
+print(phonebook)
+del phonebook["Chris"]
+print(phonebook)
 
 
 print()
@@ -80,8 +92,17 @@ print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
 
+for key in phonebook:
+    print(f'The key is: {key} and the value is {phonebook[key]}')
 
+for value in phonebook.values():
+    print(value)
 
+for key, value in phonebook.items():
+    print(f'The key is: {key} and the value is {value}')
+
+for ph_tuple in phonebook.items():
+    print(ph_tuple)
 
 
 print()
@@ -90,16 +111,17 @@ print()
 
 
 
-
-
 print()
 print('*****  start section 6 - using get and clear ********')
 print()
 
+name = 'Chris'
+
+phone = phonebook.get(name, 'key not found')
+print(phone)
 
 
-
-
+phonebook.clear()
 
 print()
 print('*****  end section 6 ********')
@@ -112,8 +134,9 @@ print('*****  start section 7 - using pop method ********')
 print()
 
 
-
-
+remove = phonebook.pop('Chris', 'not found')
+print(remove)
+print(phonebook)
 
 
 print()
@@ -121,13 +144,14 @@ print('*****  end section 7 ********')
 print()
 
 
-
 print()
 print('*****  start section 8 - using popitem ********')
 print()
 
 
-
+a = phonebook.popitem()
+print(a)
+print(phonebook)
 
 
 
@@ -136,21 +160,20 @@ print('*****  end section 8 ********')
 print()
 
 
+"""
 
 print()
 print('*****  start section 9 - using random and converting to list ********')
 print()
 
 
-
+print(phonebook[random.choice(list(phonebook))])
 
 
 print()
 print('*****  end section 9 ********')
 print()
 
-
-'''
 
 
 
--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,24 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+
+print(person)
+
+# The name of the second child
+
+print(person["children"][1])
+
+# The name of the cat
+
+print(person["pets"]["cat"])
+
+# Loop to list each child
+
+for c in person["children"]:
+    print(c)
+
+# print out the pets in this format (type:name)
+
+for k, v in person["pets"].items():
+    print(f'Tyep of pet:{k} Name of pet:{v}')
